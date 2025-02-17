d902ce72d7566746f948575f9bf80bb7e81dd100
ReoF777 <reonafuji@gmail.com>
2025-02-17 15:54:11 +0900

    更新

--- WpfApp2/Views/MainWindow.xaml.cs
+++ WpfApp2/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp2;
+namespace WpfApp2.Views;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
